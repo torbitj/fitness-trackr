bc6b2390c60a34018064bcd18646ba53ffa77e2f
JT <jstorbit@gmail.com>
2025-12-16 07:57:43 -0700

    Add error handling to show error message if user tries to delete activity they did not create

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,14 +1,25 @@
-import {useAuth} from "../auth/AuthContext"
+import { useAuth } from "../auth/AuthContext"
+import { deleteActivity } from "../api/activities";
+import { useState } from "react";
 
-export default function ActivityList({ activities }) {
+export default function ActivityList({ activities, syncActivities }) {
   const { token } = useAuth();
+  const [error, setError] = useState();
   return (
     <ul>
       {activities.map((activity) => (
         (!token)
           ? <li key={activity.id}>{activity.name}</li>
-          : <li key={activity.id}>{activity.name} <button>Delete</button></li>
+          : <li key={activity.id}>{activity.name} <button onClick={ async () => {
+            try {
+              await deleteActivity(activity.id, token)
+              syncActivities()
+            } catch (e) {
+              setError(e.message)
+            }
+          }}>Delete</button></li>
       ))}
+      {error && <p role="alert">{error}</p>}
     </ul>
   );
 }
